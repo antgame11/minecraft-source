81bd6b24197942b3fd9d6234f8ccb3724f00d08e
antgame11 <68531196+antgame11@users.noreply.github.com>
2025-10-17 21:41:50 +0000

    update

--- buildtools/source.py
+++ buildtools/source.py
@@ -1,6 +1,7 @@
 # Super bad python script!
 import os
 import requests
+import sys
 
 url = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
 response = requests.get(url)
@@ -8,14 +9,27 @@ data = response.json()
 
 latestrelease = data["latest"]["release"]
 
+# when the bruzz talm bout some enter directory with os but you js know bout some os.system :rofl:
+
+os.system(f"git switch {latestrelease} || git switch -c {latestrelease}")
+
 os.system("git clone https://github.com/FabricMC/yarn")
 
-os.system("cd yarn")
+switchbranch = os.system(f"cd yarn && git switch {latestrelease}")
 
-os.system(f"cd yarn && git checkout {latestrelease}")
+if switchbranch != 0:
+    sys.exit()
 
 os.system("cd yarn && ./gradlew mapNamedJar decompileCFR")
 
 os.system("rm -rf src")
 
 os.system("mv yarn/build/namedSrc ./src")
+
+os.system(f"""
+        git config --global user.name "github-actions"
+        git config --global user.email "actions@github.com"
+        git add .
+        git diff --cached --quiet && echo "No changes to commit." || \
+        (git commit -m "Mappings Update, {latestrelease}" && git push)
+          """)
